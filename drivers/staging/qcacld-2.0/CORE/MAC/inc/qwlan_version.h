6f4d286479c77e097d6b8aa60fc7cc67191fedbc
Skrilax_CZ <skrilax@gmail.com>
2015-06-06 12:42:51 -0700

    qcacld: Bump version to 4.3.6.108

--- drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
+++ drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            3
 #define QWLAN_VERSION_PATCH            6
 #define QWLAN_VERSION_EXTRA           ""
-#define QWLAN_VERSION_BUILD            107
+#define QWLAN_VERSION_BUILD            108
 
-#define QWLAN_VERSIONSTR               "4.3.6.107"
+#define QWLAN_VERSIONSTR               "4.3.6.108"
 
 #ifdef QCA_WIFI_2_0
 
